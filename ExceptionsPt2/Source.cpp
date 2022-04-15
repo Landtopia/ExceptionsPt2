f8a6767e240ce367e25afeae59b9fde61c93f758
Landan Allen <landan.allen@sinclair.edu>
2022-04-15 12:04:00 -0400

    I had some difficulty figuring out the math but I have the template and did more work on documenting what everything does. My output returns the values matching the homework.

--- ExceptionsPt2/Source.cpp
+++ ExceptionsPt2/Source.cpp
@@ -8,13 +8,25 @@ Date: 4/15/2022
 
 using namespace std;
 
+template <typename Temp>
+
+Temp half(Temp num) {
+    Temp type;
+    type = num / 2;
+    //Calculations, the nested if statement applies rounding rules
+    if ( *typeid(num).name() == 'i') {
+        if (type * 2 != num) {
+            type++;
+        }
+    } return type;//Returns the value to main for display
+}
 
 int main() {
-
+    
     double a = 7.0;
     float b = 5.0f;
     int c = 3;
-
+    //Displaying half the values to the user
     cout << half(a) << endl;
     cout << half(b) << endl;
     cout << half(c) << endl;
